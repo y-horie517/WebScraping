2319d0ff14ad605932aa4dc2c397a511baad56a4
y-horie517 <y.horie0517@gmail.com>
2019-05-11 02:04:08 +0900

    Requestsライブラリでサイト情報取得

--- scrapingSyllabus.py
+++ scrapingSyllabus.py
@@ -1,1 +1,12 @@
-scrapingSyllabus.py
+import requests
+
+# ページの情報を格納
+page = requests.get('https://news.yahoo.co.jp')
+
+# header情報、
+# Webページの指定encoding
+# Body以下のコンテンツ
+print(page.headers)
+print("--------")
+print(page.encoding)
+# print(page.content)
