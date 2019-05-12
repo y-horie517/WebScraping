4828235bbafff79ba1aa301b3703c3aa2ad15498
y-horie517 <y.horie0517@gmail.com>
2019-05-12 22:03:12 +0900

    カテゴリ別に記事を取得して表示

--- scrapingNews.py
+++ scrapingNews.py
@@ -1,24 +1,48 @@
 import requests
 from bs4 import BeautifulSoup
 
-# 引数はニュースカテゴリ名とそのURL
-def scrapeNews(url: str, category: str):
+# 引数はニュースカテゴリ名とそのパスの末尾
+def scrapeNews(path: str, category: str):
     # ページの情報を格納
-    res = requests.get(url)
+    url = 'https://news.yahoo.co.jp'
+
+    # pathが空でない(トップページ以外)は各カテゴリパスの記述を追加
+    if path:
+        path = '/categories/' + path
+
+    res = requests.get(url + path)
     soup = BeautifulSoup(res.text,"html.parser")
 
     for s in soup("span"):
         s.decompose()       #spanタグを抜き出して削除
 
-    print(category)
-    print('---------------------------------')
-    h1s = soup.find_all("li",class_="topicsListItem")
-    for h1 in h1s:
-        print(h1.text)
+    print(category + 'トピックス')
+    print(url + path)
     print('---------------------------------')
 
+    # 各ニュース見出しのクラス指定
+    news = soup.find_all("li",class_="topicsListItem")
+    for n in news:
+        print(n.text)
+    print('---------------------------------')
+    print()
 
-scrapeNews('http://news.yahoo.co.jp/', '主要トピックス')
+# カテゴリ名とパスの末尾部分をディクショナリに格納
+categories = {'主要':'',
+              '国内':'domestic',
+              '海外':'world',
+              '経済':'business',
+              'エンタメ':'entertainment',
+              'スポーツ':'sports',
+              'IT':'it',
+              '科学':'science',
+              '地域':'local'
+            }
+
+
+for i in categories:
+    scrapeNews(categories[i], i)
+
 
 
 
